7bcf4a32324f0476e94a7117918d4efbe768c90c
joanna-chen <joannachen.x@gmail.com>
2016-04-01 23:57:26 -0400

    list view

--- Fin/app/src/main/java/com/example/joanna/fin/DetailActivity.java
+++ Fin/app/src/main/java/com/example/joanna/fin/DetailActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,6 +16,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(new ImageAdapter(this));
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
